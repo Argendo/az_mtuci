bfc769fb9d8b21e9dd0e76db97a3b54fda4c47af
argendo <argendo@macbook.local>
2022-11-27 03:23:54 +0300

    sec_one

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'АЗ МТУСИ';
+export const SITE_DESCRIPTION = 'Здесь Вы можете узнать всю интересующую Вас информацию и характеристики актового зала МТУСИ.';
+export const HOMEPAGE_URL = 'https://www.azmtuci.ru/';
