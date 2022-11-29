6ff5f7ddf740f9686ceaa0a8c8d70726930eadbf
argendo <argendo@macbook.local>
2022-11-29 20:29:20 +0300

    width_and_header

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'АЗ МТУСИ';
+export const SITE_TITLE = 'Актовый зал МТУСИ';
 export const SITE_DESCRIPTION = 'Здесь Вы можете узнать всю интересующую Вас информацию об актовом зале МТУСИ.';
 export const HOMEPAGE_URL = 'https://azmtuci.vercel.app/';
